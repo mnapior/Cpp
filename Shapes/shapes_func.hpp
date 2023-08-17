89048c5010ab3735808b696ff2dfd8980e35fd36
mnapior <mnapior1@gmail.com>
2023-08-17 14:21:38 -0400

    RectangleAreaCalc
    Initial Shapes namespace created. Calculates area of a rectangle.

---
+++ Shapes/shapes_func.hpp
@@ -0,0 +1,10 @@
+
+namespace shapes
+{
+    float rectangleArea(float length, float height)
+    {
+        float area{0};
+        area = length * height;
+        return area;
+    }
+}
---
+++ Shapes/shapes_type.hpp
@@ -0,0 +1,11 @@
+#ifndef SHAPES_HPP
+#define SHAPES_HPP
+
+namespace shapes
+{
+    float length{0};
+    float width{0};
+    float height{0}; 
+}
+
+#endif
